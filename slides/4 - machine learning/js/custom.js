a744a8aa84427abb028bbbeb1e5107aa51ad0951
Nils Witt <n.witt@zbw.eu>
2017-04-28 10:02:57 +0200

    solutions to exercises added

---
+++ slides/4 - machine learning/js/custom.js
@@ -0,0 +1,18 @@
+/*Reveal.addEventListener( 'slidechanged', function( event ) {
+   slideNo = Reveal.getState().indexh
+   if(slideNo == 1){
+      document.getElementById('theme').setAttribute('href','css/theme/beige.css'); 
+      return false;
+   } else if(slideNo == 0){
+      document.getElementById('theme').setAttribute('href','css/theme/night.css'); 
+      return false;
+   }
+} );
+Reveal.addEventListener( 'light', function() {
+   document.getElementById('theme').setAttribute('href','css/theme/beige.css'); 
+}, false );
+
+Reveal.addEventListener( 'dark', function() {
+   document.getElementById('theme').setAttribute('href','css/theme/night.css'); 
+}, false );
+*/
